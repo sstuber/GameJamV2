f0503557c8186bfd523ba649805a2593ba323789
sstuber <stanvanmeerendonk@gmail.com>
2016-01-29 22:23:14 +0100

    Map gestart

---
+++ GameJam/Assets/Scripts/StartGrid.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGrid : MonoBehaviour {
+    public GameObject tile;
+    GameObject[,] Grid = new GameObject[16, 16];
+    // Use this for initialization
+    void Start()
+    {
+        Grid = new GameObject[16, 16];
+        for (int i = 0; i < 16; i++)
+            for (int j = 0; j < 16; j++)
+            { Grid[i, j] = Instantiate(tile);
+                Grid[i, j].transform.position = new Vector3(i, j) + tile.transform.position;
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
