cc2d9c71ac52150b7bbd07388f0a002f36a6c06f
b2om <m.j.s.boom@students.uu.nl>
2016-01-30 00:34:17 +0100

    Whatever

--- GameJam/Assets/Scripts/StartGrid.cs
+++ GameJam/Assets/Scripts/StartGrid.cs
@@ -3,21 +3,24 @@ using System.Collections;
 
 public class StartGrid : MonoBehaviour {
     public GameObject _tile;
-    public 
-    TileHandler tile;
+    public TileHandler tile;
     public int Height, Width;
     public GameObject[,] Grid;
     // Use this for initialization
-    void Start()
-    {
- 
+
+    void Awake() {
         tile = _tile.GetComponent<TileHandler>();
         Grid = new GameObject[Width, Height];
         for (int y = 0; y < Height; y++)
-            for (int x = 0; x < Width; x++)
-            { Grid[x, y]= Instantiate(_tile);
-                Grid[x, y].transform.position = new Vector3(x, y)* tile.scale + transform.position;
+            for (int x = 0; x < Width; x++) {
+                Grid[x, y] = Instantiate(_tile);
+                Grid[x, y].transform.position = new Vector3(x, y) * tile.scale + transform.position + new Vector3(0, 0, 1);
             }
+    }
+    void Start()
+    {
+ 
+        
     }
 	
 	// Update is called once per frame
---
+++ GameJam/Assets/UnitController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitController : MonoBehaviour {
+
+	// Use this for initialization
+    public InfluenceController influenceController;
+    public Vector3 target;
+    float speed = 0.1f;
+	void Start () {
+        target = transform.position + new Vector3(10, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //target = influenceController.GetBestTile();
+        float distance = (target - transform.position).magnitude;
+        print(target);
+        if (distance >= speed) {
+            transform.position += (target - transform.position).normalized * speed;
+        }
+	}
+}
