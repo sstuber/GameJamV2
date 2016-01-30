79e5c46b5d1c7cd30684cc585f67b0f9a9359b59
b2om <m.j.s.boom@students.uu.nl>
2016-01-30 02:52:14 +0100

    Menu'tje

---
+++ GameJam/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HandleClick() {
+        Application.LoadLevel("MartijnTest");
+    }
+}
