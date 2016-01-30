b2c833edab2fac4fd9d6ae1cbc19e18f2031fa97
sstuber <stanvanmeerendonk@gmail.com>
2016-01-30 23:55:04 +0100

    Valkuil sprite + unit controll

--- GameJam/Assets/Scripts/PlayerHandler.cs
+++ GameJam/Assets/Scripts/PlayerHandler.cs
@@ -7,7 +7,7 @@ public class PlayerHandler : MonoBehaviour {
     public static int manaCount;
     int manaregain = 30;
     float regainTimer =1;
-    List<GameObject>[] UnitCounts;
+    public List<GameObject> Units;
     public StartGrid startGrid;
     public int Player;
     public GameObject Unit1;
@@ -17,51 +17,46 @@ public class PlayerHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        UnitCounts = new List<GameObject>[3];
-        for (int i = 0; i < UnitCounts.Length; i++)
-            UnitCounts[i] = new List<GameObject>();
+        Units = new List<GameObject>();
         manaCount = maxMana;
         CreateUnits();
 	}
 
     void CreateUnits()
     {
-        Vector2 startPosition = new Vector2(2+ Player * (startGrid.Width - 3), (startGrid.Height - 1) / 2);
-            Unit1.GetComponent<UnitController>().StartPosition = startPosition;
+        Vector2 startPosition = new Vector2(2+ Player * (startGrid.Width - 5), (startGrid.Height - 5) / 2 -5) * StartGrid.tileScale; ;
+          //  Unit1.GetComponent<UnitController>().StartPosition = startPosition;
        //InfluenceController ic1 = 
         for (int i = 0; i < 5; i++)
         {
             //GameObject tempObj = Instantiate(Unit1);
             // tempObj.GetComponent<UnitController>().StartPosition = startPosition;
-            print(startPosition);
             var newUnit = (GameObject)Instantiate(Unit1,  startPosition + new Vector2(0, i), Quaternion.identity);
             newUnit.GetComponent<UnitController>().player = Player;
             //newUnit.GetComponent<UnitController>().influenceController = startGrid.GetComponent<InfluenceController>();
 
-            UnitCounts[0].Add(Instantiate(newUnit));
+            Units.Add(newUnit);
             
         }
-       startPosition = new Vector2(2 + Player * (startGrid.Width - 3), (startGrid.Height - 1) / 4);
+       startPosition = new Vector2(2 + Player * (startGrid.Width - 5), (startGrid.Height - 5) / 4 - 5) * StartGrid.tileScale;
         for (int i = 0; i < 5; i++)
         {
-            GameObject tempObj = Instantiate(Unit2);
-            tempObj.GetComponent<UnitController>().StartPosition = startPosition + new Vector2(0, i);
+            GameObject tempObj = (GameObject)Instantiate(Unit2, startPosition + new Vector2(0, i), Quaternion.identity);
             tempObj.GetComponent<UnitController>().player = Player;
             //tempObj.GetComponent<UnitController>().influenceController = startGrid.GetComponent<InfluenceController>();
 
-            UnitCounts[1].Add(tempObj);
+            Units.Add(tempObj);
 
         }
 
-        startPosition = new Vector2(2 + Player * (startGrid.Width - 3), (startGrid.Height - 5)  );
+        startPosition = new Vector2(2 + Player * (startGrid.Width - 5), (startGrid.Height - 10)  ) * StartGrid.tileScale; 
         for (int i = 0; i < 5; i++)
         {
-            GameObject tempObj = Instantiate(Unit3);
-            tempObj.GetComponent<UnitController>().StartPosition = startPosition + new Vector2(0,i);
+            GameObject tempObj = (GameObject)Instantiate(Unit3, startPosition + new Vector2(0, i), Quaternion.identity);
             tempObj.GetComponent<UnitController>().player = Player;
-          //  tempObj.GetComponent<UnitController>().influenceController = startGrid.GetComponent<InfluenceController>();
+            //  tempObj.GetComponent<UnitController>().influenceController = startGrid.GetComponent<InfluenceController>();
 
-            UnitCounts[2].Add(tempObj);
+            Units.Add(tempObj);
 
         }
         Tower = Instantiate(Tower);
