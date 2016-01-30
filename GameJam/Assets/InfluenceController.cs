80075b698a08f5ecf761a1668a2342dcc6255497
b2om <m.j.s.boom@students.uu.nl>
2016-01-30 02:07:38 +0100

    influence maps 1e poging

--- GameJam/Assets/InfluenceController.cs
+++ GameJam/Assets/InfluenceController.cs
@@ -9,7 +9,10 @@ public class InfluenceController : MonoBehaviour {
     public GameObject tile;
     private GameObject[,] tiles;
     StartGrid sg;
+    public bool drawInfluence = true;
+    private bool prevDraw;
 	void Start () {
+        prevDraw = drawInfluence;
         sg = transform.parent.gameObject.GetComponent<StartGrid>();
         tiles = new GameObject[sg.Width, sg.Height];
         influenceMaps = new List<InfluenceMap>();
@@ -17,11 +20,14 @@ public class InfluenceController : MonoBehaviour {
 
         for(int y = 0; y < sg.Height; ++y){
             for (int x = 0; x < sg.Width; ++x) {
-                GameObject newTile = (GameObject)Instantiate(tile, sg.Grid[x, y].transform.position * sg.tile.scale, Quaternion.identity);
+                GameObject newTile = (GameObject)Instantiate(tile, sg.Grid[x, y].transform.position, Quaternion.identity);
                 tiles[x, y] = newTile;
-                influenceMaps[0].influences[x, y] = (1 + x) * (1 + y);
+                influenceMaps[0].influences[x, y] = 0;//(1 + x) * (1 + y);
+                tiles[x, y].transform.localScale *= StartGrid.tileScale;
             }
         }
+
+        AddInfluence("goTopRight", 100f, 0.75f, 8, 8, 4);
 
         EnableDrawing(true);
 	}
@@ -37,13 +43,18 @@ public class InfluenceController : MonoBehaviour {
         for (int y = 0; y < sg.Height; ++y) {
             for (int x = 0; x < sg.Width; ++x) {
                 SpriteRenderer sr = tiles[x,y].GetComponent<SpriteRenderer>();
-                sr.color = new Color(influenceMaps[0].influences[x, y] / 256f, 0.5f, 0.5f, 0.5f) ;
+                sr.color = new Color(influenceMaps[0].influences[x, y] / 128f, 0.4f, 0.6f, 0.33f) ;
             }
         }
     }
 	// Update is called once per frame
 	void Update () {
-        ColourInfluence();
+        if (prevDraw != drawInfluence)
+            EnableDrawing(drawInfluence);
+        if(drawInfluence)
+            ColourInfluence();
+
+        prevDraw = drawInfluence;
 	}
 
     public Vector3 GetBestTile(int x, int y){
@@ -54,7 +65,7 @@ public class InfluenceController : MonoBehaviour {
                 int currentX = Mathf.Clamp(x + ix, 0, sg.Width - 1);
                 int currentY = Mathf.Clamp(y + iy, 0, sg.Height - 1);
                 float currentValue = influenceMaps[0].influences[currentX, currentY];
-                if (currentValue > bestValue) {
+                if ((currentValue * 0.9f) > bestValue) {
                     bestValue = currentValue;
                     bestX = currentX;
                     bestY = currentY;
@@ -73,6 +84,59 @@ public class InfluenceController : MonoBehaviour {
     private void AddMap(string mapName){
         InfluenceMap map = new InfluenceMap(mapName, sg.Width, sg.Height);
         influenceMaps.Add(map);
+    }
+
+    private void AddInfluence(string mapName, float power, float taper, int x, int y, int maxLoops) {
+        InfluenceMap map = findMap(mapName);
+        if (map == null)
+            return;
+
+        AddInfluenceRecursive(map, power, taper, x, y, maxLoops, 0);
+
+    }
+
+    private void AddInfluenceRecursive(InfluenceMap map, float power, float taper, int x, int y, int maxLoops, int loop = 0) {
+        if (loop > maxLoops || Mathf.Abs(power) < 1f || Mathf.Abs(power * taper) < 1f)
+            return;
+        
+        else {
+            for(int iy = -loop; iy < loop + 1; iy++){
+                int currentY = y + iy;
+                if (currentY < 0 || currentY >= sg.Height)
+                    continue;
+                if (iy == -loop || iy == loop) {
+                    for (int ix = -loop; ix < loop + 1; ix++) {
+                        int currentX = x + ix;
+                        if (currentX < 0 || currentX >= sg.Width)
+                            continue;
+
+                        map.influences[currentX, currentY] += power;
+                    }
+                }
+                else {
+                    int currentX = x - loop;
+                    if (currentX < 0 || currentX >= sg.Width)
+                        continue;
+
+                    map.influences[currentX, currentY] += power;
+                    currentX = x + loop;
+                    if (currentX < 0 || currentX >= sg.Width)
+                        continue;
+
+                    map.influences[currentX, currentY] += power;
+
+                }
+            }
+        }
+
+        AddInfluenceRecursive(map, power * taper, taper, x, y, maxLoops, loop + 1);
+    }
+
+    InfluenceMap findMap(string mapName) {
+        foreach(InfluenceMap map in influenceMaps)
+            if (map.name == mapName)
+                return map;
+        return null;
     }
 }
 
--- GameJam/Assets/Scripts/StartGrid.cs
+++ GameJam/Assets/Scripts/StartGrid.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public struct Point {
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int x, y;
+}
+
 public class StartGrid : MonoBehaviour {
     public GameObject _tile;
     public TileHandler tile;
     public int Height, Width;
     public GameObject[,] Grid;
+    public static float tileScale;
     // Use this for initialization
 
     void Awake() {
         tile = _tile.GetComponent<TileHandler>();
+        tileScale = tile.scale;
         Grid = new GameObject[Width, Height];
         for (int y = 0; y < Height; y++)
             for (int x = 0; x < Width; x++) {
@@ -17,6 +27,15 @@ public class StartGrid : MonoBehaviour {
                 Grid[x, y].transform.position = new Vector3(x, y) * tile.scale + transform.position + new Vector3(0, 0, 1);
             }
     }
+
+
+    public static Point PositionToGridIndex(Vector3 pos) {
+        int x = (int)(pos.x / tileScale);
+        int y = (int)(pos.x / tileScale);
+        return new Point(x, y);
+    }
+
+
     void Start()
     {
  
