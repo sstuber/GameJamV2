82378fcd8a765c1fae7189a3bd364c566add1d24
Sem Berretty <semberretty@gmail.com>
2016-01-30 11:29:23 +0100

    Towers voor beide kanten met een gem om team kleuren te defineren

---
+++ GameJam/Assets/Scripts/FloatingGem.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingGem : MonoBehaviour {
+
+    Vector2 GemMoveSpeed;
+    public GameObject Gem;
+    public float UpperLimit;
+    public float BottomLimit;
+    bool MoveGemUp;
+    bool MoveGemDown;
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+
+        GemMoveSpeed = new Vector2(0,Speed);
+        MoveGemUp = true;
+       
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Gem.GetComponent<Rigidbody2D>().position.y >= UpperLimit)
+        {
+            MoveGemUp = false;
+            MoveGemDown = true;
+        }
+
+        if (Gem.GetComponent<Rigidbody2D>().position.y <= BottomLimit)
+        {
+            MoveGemUp = true;
+            MoveGemDown = false;
+        }
+
+
+        if (MoveGemUp)
+        {
+            Gem.GetComponent<Rigidbody2D>().position += GemMoveSpeed;
+        }
+
+        if (MoveGemDown)
+        {
+            Gem.GetComponent<Rigidbody2D>().position -= GemMoveSpeed;
+        }
+
+	}
+
+    
+}
